1f3f4bb8cf99c70d05ac5ccc18bbfbaf247f553f
CamachoDejay <camachodejay@yahoo.com>
2017-12-06 17:16:02 +0100

    functionality needed to test the code

--- GradientFit/example.m
+++ GradientFit/example.m
@@ -26,8 +26,22 @@ threshold = 100;  %
 GraR = 3; % The radius of Gradient used for caculation
 spotNUM = size(ROIs,3);
 spot3D = zeros(3,1);
+old = load('originalOUT.mat');
+
 for N=1:spotNUM
-    [x,y,e] = GradientFit(ROIs(:,:,N),RegR,GraR);
+    [x,y,e] = GradientFit(ROIs(:,:,N),GraR);
+    % reporting differences
+    
+    diffE = abs(e-old.e);
+    fprintf('original e: %.4g ;\t new e: %.4g ;\t difference: %.4g\n',old.e,e,diffE)
+
+    diffX = abs(x-old.x);
+    fprintf('original x: %.4g ;\t new x: %.4g ;\t difference: %.4g\n',old.x,x,diffX)
+
+    diffY = abs(y-old.y);
+    fprintf('original y: %.4g ;\t new y: %.4g ;\t difference: %.4g\n',old.y,y,diffY)
+
+    
     xc = (ROI_coor(2,N) + x)*PixelSize;
     yc = (ROI_coor(1,N) - y)*PixelSize;
     zc = CCP(1)*e^4 + CCP(2)*e^3 + CCP(3)*e^2 + CCP(4)*e + CCP(5);
---
+++ GradientFit/gaus2D.m
@@ -0,0 +1,18 @@
+function [G] = gaus2D(pos, sig, xid,yid)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+A = 100;
+[x,y] = meshgrid(xid,yid);
+sigX = sig(1);
+sigY = sig(2);
+x0 = pos(1);
+y0 = pos(2);
+
+xPart = ((x-x0).^2) ./ (2*sigX^2);
+yPart = ((y-y0).^2) ./ (2*sigY^2);
+
+
+G = A.*exp( -(xPart + yPart));
+
+end
+
