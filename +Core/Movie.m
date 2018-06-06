286987ea610866488d9b54a91d5b859ce47c2ce6
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2018-06-06 19:17:05 +0200

    Commented set.status, added the possibility for the user to give only a path (not only struct) as input to the constructor for the relevant parameters, finished the updateStatus method

--- +Core/Movie.m
+++ +Core/Movie.m
@@ -25,34 +25,39 @@ classdef Movie <  handle
             %Give a status depending on input.
             switch nargin
                 case 1 
-                    obj.status = 'raw';
                 case 2
                     obj.info = info;
-                    obj.status = 'raw';
                 case 3
                     obj.info = info;
                     obj.cal = cal;
-                    obj.status = 'rawCal';
                 case 4
                      obj.cal = cal;
                      obj.info = info;
                      obj.calibrated = calibrated;
-                     obj.status = 'calibrated';
                 case 5
                      obj.cal = cal;
                      obj.info = info;
                      obj.calibrated = calibrated;
                      obj.superResCal = superResCal;
-                     obj.status = 'SRcalibrated';
                 otherwise 
                     error('Unexpected number of argument');
             end
+            obj.updateStatus;
         end
         
         function  set.raw(obj,raw)
+            %Check if the raw is a struct(full info) or a char
+            %(only path)
+            if isstruct(raw)
+            elseif ischar(raw)
+                path = raw;
+                clear raw
+                raw.path = path;
+            end
+            %Thorough check of the raw
             [checkRes] = obj.checkRaw(raw);
-            %When every check/modification is performed, raw is finally
-            %store into the movie object
+            %Depending on how the check went we propose the user to 
+            %recalculate the parts that are wrong.
             if checkRes == false
                 answer = questdlg('Path of raw file seems fine but something is wrong with the info, should we re-extract it?',...
                     'Question to user','Yes','No','Yes');
@@ -63,22 +68,29 @@ classdef Movie <  handle
                     case 'No'
                         error('Something is wrong with raw file, request aborted.')
                 end
-                
             end
             obj.raw = raw;
         end
         
         function set.info(obj,info)
-            %TO DO write the needed assertion.
             [checkRes] = obj.checkInfo(info);
             if checkRes
                 obj.info = info;
             else
-                error('Something is wrong with the format of the info provided by the user')
+                warning('Something is wrong with the format of the info provided by the user, storage aborted')
             end
         end
         
         function set.cal(obj,cal)
+            %Check if the calibration is a struct(full info) or a char
+            %(only path)
+            if isstruct(cal)
+            elseif ischar(cal)
+                path = cal;
+                clear raw
+                cal.path = path;
+            end
+            
             [checkRes] = obj.checkCal(cal);
             if checkRes == false
                 answer = questdlg('Path of calibration seems fine but something is wrong with the info, should we re-extract it?',...
@@ -94,16 +106,15 @@ classdef Movie <  handle
             end
             obj.cal = cal;
         end
-        
-        function set.status(obj,status)
-            %TO DO Check with Rafa how 
-            [checkRes] = obj.checkStatus(status);
-            if checkRes
-            obj.status = status;
-            else
-                error('There is a mismatch between the current experimental status and the content of the movie object, please check.')
-            end 
-        end
+        %What to do with this one?
+%         function set.status(obj,status)
+%             [checkRes] = obj.checkStatus(status);
+%             if checkRes
+%             obj.status = status;
+%             else
+%                 error('There is a mismatch between the current experimental status and the content of the movie object, please check.')
+%             end 
+%         end
 
         function set.calibrated(obj,calibrated)
             %TO DO write the needed assertion.
@@ -156,7 +167,7 @@ classdef Movie <  handle
             %Extract the fields names
             fields = fieldnames(cal);
             %Check that the first fields is path
-            assert(and(isfield(cal,'path'),~isempty(cal.cal)),...
+            assert(and(isfield(cal,'path'),~isempty(cal.path)),...
                     'no filepath provided in Cal');
             %Check that the file path exist
             assert(exist(cal.path,'file') == 2,...
@@ -164,6 +175,9 @@ classdef Movie <  handle
          
             if (length(fields)<3)
             else
+                %TODO: Making it more readable?
+                %check if the field exist, if the field is a struct and if
+                %it has the expected number of fields.
                 if(or(and(isfield(cal,'info'),isfield(cal,'file')),...
                         or(and(~isstruct(cal.info),...
                         or(isempty(cal.info),numel(fieldnames(cal.info)) ~= 6)),...
@@ -216,7 +230,8 @@ classdef Movie <  handle
         
         
         function updateStatus(obj)
-            
+            %Cascade, as long as nothing empty is found we keep looking at
+            %the next element to know which status it is. 
             if(isempty(obj.raw))
                 obj.status = 'none';
             elseif (isempty(obj.cal))
@@ -225,8 +240,21 @@ classdef Movie <  handle
                 obj.status = 'rawCalc';
             elseif (isempty(obj.superResCal))
                 obj.status = 'calibrated';
-            else
+            elseif (~isempty(obj.superResCal))
                 obj.status = 'SRCalibrated';
+            else
+                error('Whooop something is wrong with status, go in the corner and think about what you just did!')
+            end
+            %if the cascade stop in some places we still check that
+            %calibrated and SR calibrated are not empty because we allow
+            %the user to load from the calibrated data directly(without
+            %raw)
+            if (~isempty(obj.calibrated))
+                if(~isempty(obj.superResCal))
+                    obj.status = 'SRCalibrated';
+                else
+                    obj.status = 'calibrated';
+                end
             end
 
         end
