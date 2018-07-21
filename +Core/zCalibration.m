4b8919bc52408c877340800a67f1e462f3b7cb90
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2018-07-21 15:19:25 +0200

    Corrected a few errors from sloppy work the other day and clean up

--- +Core/zCalibration.m
+++ +Core/zCalibration.m
@@ -25,7 +25,7 @@ classdef zCalibration < handle
             folder2Mov = dir(obj.path);
             %loop through the content of the directory
             for i = 3:size(folder2Mov,1)
-                %If element i is a folder 
+                %If element i is a folder
                 if folder2Mov(i).isdir
                     %Check if the directory
                     currDir = dir([folder2Mov(i).folder filesep folder2Mov(i).name]);
@@ -68,64 +68,36 @@ classdef zCalibration < handle
             
             nfields = numel(fieldnames(obj.zCalMovies));
             allData = cell(8,3);
+            %We regroup the data per plane of different movie together
             for i = 1: nfields
                 
                 for j = 1: length(obj.zCalMovies.(['zCal' num2str(i)]).zCalibration.syncEllip)
                     
                     allData{j,1} = [allData{j,1} ;obj.zCalMovies.(['zCal' num2str(i)]).zCalibration.syncEllip{j,1} ];
-                    
-                    
+                    allData{1,2} = [allData{1,2} ;obj.zCalMovies.(['zCal' num2str(i)]).zCalibration.syncEllip{1,2} ];
                 end
                 
             end
+            %Sort the data
             allData{1,3} = obj.zCalMovies.(['zCal' num2str(i)]).zCalibration.syncEllip{1,3};
             for j = 1: length(obj.zCalMovies.(['zCal' num2str(i)]).zCalibration.syncEllip)
                 [~,ind] = sort(allData{j,1}(:,1));
                 allData{j,1} = allData{j,1}(ind,:);
                 
             end
-            
+            %Sort all data together
+            [~,ind] = sort(allData{1,2}(:,1));
+            allData{1,2} = allData{1,2}(ind,:);
+            %Fitting of the data occurs here
             zCalibration = obj.calZCalibration(allData);
-            
+            %storage to the object
             obj.calib.file = zCalibration;
             obj.calib.data = allData;
             
         end
         
-        function [zCalibration] = calZCalibration(obj,zSyncCalData)
-            %function that take the synchronized z-ellip Data and fit, for
-            %each planes with a polynomial. It stores the coeff of the
-            %polynomials
-            zCalibration = cell(length(zSyncCalData),1);
-            deg = zSyncCalData{1,3}(3);
-            
-            for i = 1: length(zSyncCalData)
-                dataCurrentPlane = zSyncCalData{i};
-                [binnedData] = Plotting.qBinning(dataCurrentPlane,length(dataCurrentPlane)/5);
-                
-                zVec = min(dataCurrentPlane(:,1)):max(dataCurrentPlane(:,1));
-     
-                p = polyfit(dataCurrentPlane(:,1),dataCurrentPlane(:,2),deg);
-                fit = polyval(p,zVec);
-%                 
-%                 figure
-%                 subplot(1,2,1)
-%                 hold on
-%                 scatter(binnedData(:,1),binnedData(:,2))
-%                 plot(zVec,fit)
-%                 
-%                 subplot(1,2,2)
-%                 hold on
-%                 scatter(dataCurrentPlane(:,1),dataCurrentPlane(:,2))
-%                 plot(zVec,fit)
- 
-                zCalibration{i} = p;
-                
-            end
-            
-        end
-        
         function showZCalibration(obj)
+            
             assert(~isempty(obj.calib),'No zzCalibrationration found, please run z zCalibrationration before display');
             relZ = obj.zCalMovies.('zCal1').calibrated.oRelZPos*1000;%in nm
             figure()
@@ -139,26 +111,26 @@ classdef zCalibration < handle
                 
                 ellip2 = 1./ellip(ellip<=1);
                 z2 = z(ellip<=1);
-               yAx = 1:0.1:2;
-               subplot(2,1,1)
-               hold on
-               scatter(z1,ellip1)
-               plot(ones(length(yAx),1)*relZ(i),yAx)
-               title('Elliptiticy Elongated in Y')
-               xlabel('zPos (nm)')
-               ylabel('Ellipticity (sigY/sigX)')
-               ylim([1 2])
-               hold off
-               
-               subplot(2,1,2)
-               hold on
-               scatter(z2,ellip2)
-               plot(ones(length(yAx))*relZ(i),yAx)
-               title('Elliptiticy Elongated in X')
-               xlabel('zPos (nm)')
-               ylabel('Ellipticity (sigX/sigY)')
-               ylim([1 2])
-               hold off
+                yAx = 1:0.1:2;
+                subplot(2,1,1)
+                hold on
+                scatter(z1,ellip1)
+                plot(ones(length(yAx),1)*relZ(i),yAx)
+                title('Elliptiticy Elongated in Y')
+                xlabel('zPos (nm)')
+                ylabel('Ellipticity (sigY/sigX)')
+                ylim([1 2])
+                hold off
+                
+                subplot(2,1,2)
+                hold on
+                scatter(z2,ellip2)
+                plot(ones(length(yAx))*relZ(i),yAx)
+                title('Elliptiticy Elongated in X')
+                xlabel('zPos (nm)')
+                ylabel('Ellipticity (sigX/sigY)')
+                ylim([1 2])
+                hold off
                 
             end
             
@@ -177,11 +149,11 @@ classdef zCalibration < handle
                 [binnedData] = Plotting.qBinning(dataCurrentPlane,length(dataCurrentPlane)/5);
                 zVec = min(dataCurrentPlane(:,1)):max(dataCurrentPlane(:,1));
                 %retrieving fit to display
-                p = obj.calib.cal{i};
+                p = obj.calib.file{i};
                 fit = polyval(p,zVec);
                 %shifting according to the plane
                 zVec = zVec + relZ(i) ;
-                dataCurrentPlane(:,1) = dataCurrentPlane(:,1)+ relZ(i); 
+                dataCurrentPlane(:,1) = dataCurrentPlane(:,1)+ relZ(i);
                 binnedData(:,1) = binnedData(:,1) +relZ(i);
                 
                 subplot(1,2,1)
@@ -189,7 +161,7 @@ classdef zCalibration < handle
                 scatter(binnedData(:,1),binnedData(:,2))
                 plot(zVec,fit)
                 title('Binned data fitted with polynomial')
-
+                
                 subplot(1,2,2)
                 hold on
                 scatter(dataCurrentPlane(:,1),dataCurrentPlane(:,2))
@@ -198,8 +170,35 @@ classdef zCalibration < handle
                 
             end
             
-
+            
         end
+        
+    end
+    
+    methods (Access = private)
+        
+        function [zCalibration] = calZCalibration(~,zSyncCalData)
+            %function that take the synchronized z-ellip Data and fit, for
+            %each planes with a polynomial. It stores the coeff of the
+            %polynomials
+            zCalibration = cell(length(zSyncCalData),1);
+            deg = zSyncCalData{1,3}(3);
+            
+            for i = 1: length(zSyncCalData)
+                dataCurrentPlane = zSyncCalData{i};
+                [binnedData] = Plotting.qBinning(dataCurrentPlane,length(dataCurrentPlane)/5);
+                
+                zVec = min(dataCurrentPlane(:,1)):max(dataCurrentPlane(:,1));
+                
+                p = polyfit(dataCurrentPlane(:,1),dataCurrentPlane(:,2),deg);
+                fit = polyval(p,zVec);
+                
+                zCalibration{i} = p;
+                
+            end
+            
+        end
+        
     end
 end
 
