0c18a13f2b24e8c152d3105fb6b992669f016b0f
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2018-09-10 20:01:32 +0200

    test file for MPLocMov functions

---
+++ testmpLocMov.m
@@ -0,0 +1,43 @@
+%%
+clc 
+close all
+clear 
+
+path2ZCal = 'E:\Data\Leuven Data\2018\03-Mar\22\ZCalibration\';
+path2SRCal = 'E:\Data\Leuven Data\2018\03-Mar\22\PlaneCaibration\';
+
+path2File = 'E:\Data\Leuven Data\2018\03-Mar\22\ZCalibration\BeadsZCalibration_2';
+path2Cal = '..\data\Multiplane\PlaneCalib\BeadsCalibrationZStack_1';
+
+detectParam.delta = 6;
+detectParam.chi2 = 80;
+%%
+calib = Core.MPCalibration(path2Cal);
+
+%%
+
+MPLocMov = Core.MPLocMovie(path2File,calib.getCal,path2SRCal,path2ZCal);
+
+%% Detection
+
+MPLocMov.giveInfo
+%find candidate
+MPLocMov.findCandidatePos(detectParam);
+
+%fit position
+MPLocMov.SRLocalizeCandidate;
+
+%% Data correction
+rot = false;
+refPlane = 5;
+MPLocMov.applySRCal(rot,refPlane);
+%% e-Z transformation
+MPLocMov.applyZCal;
+
+%% Plane consolidation
+
+MPLocMov.consolidatePlanes
+
+%% plot
+
+MPLocMov.showCorrLoc;
