2def617997e7e489664afb760d0fff10e28f1f71
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2018-12-10 15:49:54 +0100

    Minor fixing to allow for loading different types of data

--- mainKnownNumberOfParticles.m
+++ mainKnownNumberOfParticles.m
@@ -2,48 +2,49 @@ clear
 clc
 close all
 %% User input
-delta = 50;
-nParticles = 2;
-pxSize = 95;
-minDist = 6; %in pixels
+delta = 50;% in px Size of the ROI around particles detected(radius 50 = 100x100 pixel
+nParticles = 2;%number of particles expected in the movie has to be exact
+pxSize = 95;%in nm
+minDist = 6; %in pixels (min distant expected between particles
 scaleBar = 2; %in um
-tail = 20;
-info.type = 'transmission';
-
-toAnalze = 'folder';
-outputFolder = 'Results';
-%% Create the Movie object
-switch toAnalze
+tail = 20;%Length of the tail in frames, for plotting the traces on top of the movie
+info.type = 'transmission';%Transmission or normal movie
+frameRate = 30; %for saving the movie
+toAnalyze = '.mp4';%accepted: .mp4, .ome.tif, folder. (folder that contain folders of .ome.tif.
+outputFolder = 'Results';%name of the folder to output the results
+%% Loading
+switch toAnalyze %switch depending on what user want to analyze
     case '.mp4'
-        [folder2Mov] = Core.Movie.getFileInPath(path2File, toAnalze);
-        p2file = [folder2Mov.folder filesep folder2Mov.name];
-                 
-    case '.ome.tif'
-        [folder2Mov,path,outDir] = Load.Folder(toAnalze,outputFolder);
+        %load all .mp4 in the selected folder
+        [folder2Mov,path,outDir] = Load.Folder(toAnalyze,outputFolder);
         mkdir(outDir);
-        
-        
+    case '.ome.tif'
+        %load all .ome.tif in the selected folder
+        [folder2Mov,path,outDir] = Load.Folder(toAnalyze,outputFolder);
+        mkdir(outDir);
+
     case 'folder'
-        [path] = uigetdir();
-        tmp = dir(path);
-        tmp = tmp(cell2mat({tmp.isdir}));
-        tmp = tmp(3:end);
+        [path] = uigetdir();%allow user to select folder
+        tmp = dir(path);%make it a directory in matlab (list of all thing contained inside)
+        tmp = tmp(cell2mat({tmp.isdir}));%only keep the subfolders
+        tmp = tmp(3:end);%remove the access to parent folders
         folder2Mov = [];
         for i = 1:size(tmp,1)
-            path2File = [tmp(i).folder filesep tmp(i).name];
-            file2Analyze = Core.Movie.getFileInPath(path2File,'.ome.tif');
-            
-            folder2Mov = [folder2Mov file2Analyze];
+            path2File = [tmp(i).folder filesep tmp(i).name];%get path to subfolders
+            file2Analyze = Core.Movie.getFileInPath(path2File,'.ome.tif');%get .ome.tif in subfolder
+            folder2Mov = [folder2Mov file2Analyze];%store the info to the file if it found one (otherwise file2Analyze is empty)
         end
-        outDir = [path filesep 'Results'];   
+        %create the out directory
+        outDir = [path filesep 'Results']; 
+        mkdir(outDir)
     otherwise
-        error('Unknown extension');
-
+        error('Unknown extension');%throw an error if none of the above possibilities is given
 
 end
-
+%check that folder2Mov is not empty
 assert(~isempty(folder2Mov),'Error no file was found, check that you put the correct analysis type');
 %% detection of the center of the beads
+%preallocate memory for storing data of the different files
 allData = struct('fileName',[],'locPos',[]);
 allData(size(folder2Mov,2)).locPos = [];
 
@@ -52,79 +53,91 @@ for i =1: size(folder2Mov,2)
     currentPath = folder2Mov(i).folder;
     switch toAnalyze
         case '.mp4'
-            v = VideoReader(p2file);
-            nFrames = floor(v.Duration*v.FrameRate);
-            fullStackIn = zeros(v.Height,v.Width,nFrames);
+            p2file = [folder2Mov(i).folder filesep folder2Mov(i).name];
+            v = VideoReader(p2file);%Create VideoReader object
+            nFrames = floor(v.Duration*v.FrameRate);%extract the number of frames
+            fullStackIn = zeros(v.Height,v.Width,nFrames);%preallocate memory
             for j = 1:nFrames
-                frame = readFrame(v);
+                frame = readFrame(v);%read frames
 
-                fullStackIn(:,:,j) = rgb2gray(frame);
+                fullStackIn(:,:,j) = rgb2gray(frame);%extract the intensity data from frames
             end
         otherwise
-            myMov = Core.Movie(currentPath,info,'.tif');
-            fullStack = myMov.getFrame(1);
+            myMov = Core.Movie(currentPath,info,'.tif');%Create Movie Object
+            fullStack = myMov.getFrame(1);%extract first frame
             frame = fullStack.Cam1;
+            %check if cropping is necessary
             if size(frame,2) > 400
                 myMov.cropIm;
             end
+            %load full stack
             fullStack = myMov.getFrame;
-
+            %revert the intensity scale
             fullStackIn = imcomplement(fullStack.Cam1);
     end
-            frame = 5;
-
+    
+    frame = 5;
+    %get the n maxima where n is the number of particles expected and
+    %minDist is the distance expected between them
     [pos] = goldProj.nMaxDetection (fullStackIn(:,:,frame),nParticles,minDist);
-
+    
     x0 = pos(:,2);
     y0 = pos(:,1);
 
     pos = [y0 x0];
+    %calculate center of mass of particles position for cropping later
     cropPos = round(mean(pos,1));
-
+    %plot to check that the max detection worked
     figure
     imagesc(fullStackIn(:,:,frame))
     hold on
     plot(pos(:,2),pos(:,1),'r+')
 
     %% Cropping Movie
-
+    %crop the desired region around the center of mass of particles located
+    %using delta provided by user
     fullStackIn = fullStackIn(cropPos(1)-delta:cropPos(1)+delta, cropPos(2)-delta:cropPos(2)+delta,:);
-    %% Data manipulation
+    %% Fitting
+    
     nFrames = size(fullStackIn,3);
+    %get the X Y dommain
     x = 1:size(fullStackIn,2);
     y = 1:size(fullStackIn,1);
     [domX,domY] = meshgrid(x,y);
     dom(:,:,1) = domX;
     dom(:,:,2) = domY;
-
+    %preallocate memory
     data2Store = zeros(nFrames,2,nParticles);
     fitMov = zeros(size(fullStackIn));
-    h = waitbar(0,'Fitting Data');
+    h = waitbar(0,'Fitting Data');%create waiting bar
 
     for j = 1:nFrames
         currentFrame = double(fullStackIn(:,:,j));
-
-    [pos] = goldProj.nMaxDetection (currentFrame,nParticles,minDist);
+        %inital detection of particles on currentFrame
+        [pos] = goldProj.nMaxDetection (currentFrame,nParticles,minDist);
 
         x0 = pos(:,2);
         y0 = pos(:,1);
-
+        %Multiple gaussian fitting occurs here
         [gPar,resnorm,res] = Localization.Gauss.MultipleFitting(currentFrame,x0,y0,dom,nParticles); 
+        %Generate an image of the Fit to be able to plot in case we want to
+        %check
         F = Localization.Gauss.MultipleGauss(gPar, dom,nParticles);
 
         if j>1
-
+            %Tracking based on MSD minimization 
             newOrder = goldProj.simpleTracking(gPar(5:end),prevPos);
-            %reshaping and sorting with new order
+            %reshaping to format the final data and sorting with new order
             gPos = reshape(gPar(5:end),[2,nParticles]);
             gPos = gPos(:,newOrder);
             prevPos = reshape(gPos,[1,nParticles*2]);
             gPos = reshape(gPos,[1,2,nParticles]);
+            %store data
             data2Store(j,:,:) = gPos;
             clear gPos
 
         else
-
+            %First frame we just reshape
             gPos = reshape(gPar(5:end),[2,nParticles]);
             gPos = reshape(gPos,[1,2,nParticles]);
             data2Store(j,:,:) = gPos;
@@ -132,20 +145,26 @@ for i =1: size(folder2Mov,2)
             prevPos = gPar(5:end);
             clear gPos
         end
-
+        %store fittings
         fitMov(:,:,j) = F;
+        %update waitbar value
         waitbar(j/nFrames,h,'Fitting Data')
     end
-        
+    %save data to the current folder being analyze
     filename = [currentPath filesep 'LocalizationData.mat'];
     save(filename,'data2Store');
+    %store data in allData
     allData(i).locPos = data2Store;
     allData(i).fileName = currentPath;
+    %clear waitbar
     close(h);
     
     %% display figure
+    %switch from pixel to nm
     data2plot = data2Store *pxSize;
+    %calculate mean center of mass of all particles
     cm = [mean(mean(data2plot(:,1,:))) mean(mean(data2plot(:,2,:)))];
+    %plot localization data
     Fig = figure;
     hold on
     for j = 1 : nParticles
@@ -156,25 +175,24 @@ for i =1: size(folder2Mov,2)
     xlabel('X Position (px, 1px = 95nm)')
     ylabel('Y Position (px, 1px = 95nm)')
     title('All localized spot');
+    %save the figure in the current folder path
     filename = [currentPath filesep 'LocalizationDensity.fig'];
     saveas(Fig,filename);
     
     % Cropping full stack
-switch toAnalze
+switch toAnalyze
     case '.mp4'
         fullStackNorm = fullStackIn;
     otherwise
         fullStackNorm = imcomplement(fullStackIn);
 end
 %% MovieMaker
-
-frameRate = 30;
+%save a movie where the traces is displayed on top of the image
 filename = [currentPath filesep 'TrackMovie.gif'];
-
 goldProj.makeTraceMovie(data2Store,fullStackNorm,filename,frameRate,scaleBar,tail);
 
 end
-
+%save all Data in the master folder
 filename = [outDir filesep 'allLoc'];
 save(filename,'allData');
 
