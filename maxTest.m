4ddefd3b00687622f6b95e0e336178f06bc3db2f
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2018-12-12 09:40:08 +0100

    Now segmenting the image excluding the cell in the center

--- maxTest.m
+++ maxTest.m
@@ -11,44 +11,96 @@ frame = 1:info.Frame_n;
 
 IM = Load.Movie.tif.getframes(path2File,frame);
 
-figure
-
+figure(1)
+title('Detecting cell countour')
+subplot(2,4,1)
 imagesc(IM)
+title('Raw Data')
 colormap('hot')
+axis image
 %% Segmenting cell
 
 BW = imbinarize(IM,threshold);
 
-figure
-
+figure(1)
+subplot(2,4,2)
 imagesc(BW)
+title('Binary image')
 colormap('hot')
+axis image
 
 BWL = bwlabel(BW);
 
-test = regionprops(BW,'Area');
-[biggestArea,idx] = max(cell2mat({test.Area}));
+%Get the largest area
+BWarea = regionprops(BW,'Area');
+[~,idx2BiggestArea] = max(cell2mat({BWarea.Area}));
 
-BW(BWL~=idx) = 0;
+%kill all the other area found
+BW(BWL~=idx2BiggestArea) = 0;
 
 % Clean up boundary
 se = strel('disk',10);
 BW = imclose(BW,se);
 
-
-figure(3)
+%plotting
+figure(1)
+subplot(2,4,3)
 imagesc(BW)
 hold on
 colormap('hot')
-
+axis image
+title('Cleaned binary image')
 contour = bwboundaries(BW);
-figure(4)
+
+%find largest contour
+contourSize = cellfun(@size,contour,'UniformOutput',false);
+[tmp] = cellfun(@max,contourSize,'UniformOutput',false);
+[~,idx2OuterContour] = max(cell2mat(tmp));
+%only keep largest countour
+contour = contour{idx2OuterContour};
+
+figure(1)
+subplot(2,4,4)
 imagesc(IM)
 hold on
-plot(contour{1}(:,2),contour{1}(:,1),'w','LineWidth',3)
+plot(contour(:,2),contour(:,1),'w','LineWidth',3)
 colormap('hot')
-
+axis image
+title('Cell Countour')
 %% Segmenting the rest of the image 
+
+mask = poly2mask(contour(:,2),contour(:,1),size(IM,1),size(IM,2));
+mask = ~mask;
+figure(1)
+subplot(2,4,5)
+imagesc(mask)
+colormap('hot');
+axis image;
+title('Mask');
+
+disp('Now doing 3D gauss filtering this can take about 3 minutes')
+
+% size of gauss filter
+S = 1;
+% size of pixel in z vs x/y
+pixZ  = 4;
+zFactor = 2;
+sigma = [S,S,S*zFactor/pixZ];
+IMs = imgaussfilt3(IM, sigma);
+disp('DONE with filtering ------------')
+
+diskDim = 4;
+[gBW,aBW] = imSegmentation.segmentStack(IMs,'threshold',threshold,'diskDim',diskDim);
+
+BWadapt  = aBW.*mask;
+BWglobal = gBW.*mask;
+
+figure(1)
+subplot(2,4,6)
+imagesc(BWadapt)
+colormap('hot');
+axis image;
+title('Mask');
 
 %% calculate the ellipticity-distance relationship
 
