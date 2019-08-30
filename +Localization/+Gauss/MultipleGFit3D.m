09f53ff5601a3ef40c6223abd80c8644ad901662
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2019-08-30 17:45:10 +0200

    Added function to fit 3D Gaussian

---
+++ +Localization/+Gauss/MultipleGFit3D.m
@@ -0,0 +1,109 @@
+%Fitting
+function [gpar,resnorm,res] = MultipleGFit3D(A,X0,Y0,Z0,domain,NbG,width)
+maxNFit = 5;
+if NbG > maxNFit
+    error('too many PSF are asked, maximum is 8 for fitting')
+end
+if length(X0) < maxNFit
+    X0(length(X0)+1:maxNFit) = 0;
+    Y0(length(Y0)+1:maxNFit) = 0;
+    Z0(length(Z0)+1:maxNFit) = 0;
+end
+
+if width.xy >0
+    
+    wguessXY = width.xy;
+    lwXY = width.xy;
+    uwXY = width.xy;
+    
+    wguessZ = width.z;
+    lwZ = width.z;
+    uwZ = width.z;
+    
+else
+    wguessXY = 3;
+    lwXY = 1;
+    uwXY = 5;
+    
+    wguessZ = 2;
+    lwZ = 0.5;
+    uwZ = 5;
+end
+
+bkgub = 0.9  *  max(A(:));
+bkglb = 0.95 * min(A(:));
+Bkg0 = median(A(:));
+% PSFMicroscope=350; %nm
+% PSF=PSFMicroscope/160/2.355; %pixel
+
+curvefitoptions = optimset('Display','off');
+% LOWER BOUND FOR FITTING PARAMETER
+%[peak Int,     sigma X,    sigma Y,        Bkg,            mean X,         mean Y
+lb        = [0.5*(max(A(:))-min(A(:)))              lwXY      lwZ         bkglb...
+       min(domain(:))        min(domain(:))     min(domain(:))        min(domain(:))...
+       min(domain(:))        min(domain(:))     min(domain(:))        min(domain(:))...
+       min(domain(:))        min(domain(:))     min(domain(:))        min(domain(:))...
+       min(domain(:))        min(domain(:))     min(domain(:))]; 
+% UPPER BOUND FOR FITTING PARAMETER
+ub        = [1.2*(max(A(:)))  uwXY      uwZ        bkgub ...
+    max(domain(:))  max(domain(:))  max(domain(:))    max(domain(:))...
+    max(domain(:))  max(domain(:))  max(domain(:))    max(domain(:))...
+    max(domain(:))  max(domain(:))  max(domain(:))    max(domain(:))...
+    max(domain(:))  max(domain(:))  max(domain(:))];
+
+lb = lb(1,1:4+(NbG)*3);
+ub = ub(1,1:4+(NbG)*3);
+
+%INITIAL GUESS
+initguess = [max(A(:))-min(A(:))     wguessXY      wguessZ          Bkg0  ...
+            X0(1)          Y0(1)    Z0(1) ...
+            X0(2)          Y0(2)    Z0(2) ...
+            X0(3)          Y0(3)    Z0(3) ... 
+            X0(4)          Y0(4)    Z0(4) ...
+            X0(5)          Y0(5)    Z0(5) ];
+%             X0(5)          Y0(5)    X0(6)      Y0(6) ...
+%             X0(7)          Y0(7)    X0(8)      Y0(8)];
+        
+initguess = initguess(1,1:4+(NbG)*3);
+
+switch NbG
+    case 1
+           
+        [gpar,resnorm,res] = lsqcurvefit(@Gauss3D,...
+            initguess,domain,A,lb,ub,curvefitoptions);
+        
+    case 2
+     
+        [gpar,resnorm,res] = lsqcurvefit(@Gauss3D2,initguess,...
+            domain,A,lb,ub,curvefitoptions);
+        
+    case 3
+    
+       [gpar,resnorm,res] = lsqcurvefit(@Gauss3D3,initguess,...
+           domain,A,lb,ub,curvefitoptions);
+        
+    case 4
+   
+        [gpar,resnorm,res] = lsqcurvefit(@Gauss3D4,initguess,...
+            domain,A,lb,ub,curvefitoptions);
+        
+    case 5
+     
+        [gpar,resnorm,res] = lsqcurvefit(@Gauss3D5,initguess,...
+            domain,A,lb,ub,curvefitoptions);
+        
+%     case 6
+%                 
+%         [gpar,resnorm,res] = lsqcurvefit(@Gauss2D6,initguess,...
+%             domain,A,lb,ub,curvefitoptions);
+%     case 7
+%          [gpar,resnorm,res] = lsqcurvefit(@Gauss2D7,initguess,...
+%             domain,A,lb,ub,curvefitoptions);
+%     case 8
+%          [gpar,resnorm,res] = lsqcurvefit(@Gauss2D8,initguess,...
+%             domain,A,lb,ub,curvefitoptions);
+    otherwise
+        error('The current version of the program cannot fit more than 8 2D gaussian at a time ');
+end
+
+end
---
+++ mainGold3Dv2.m
@@ -0,0 +1,229 @@
+%% GOLD TRACKiNG
+clear 
+clc
+close all
+
+%% User input
+
+path2Cal  = 'D:\Documents\Unif\PhD\2019-Data\08 - Aug\test3D Fitting\2DCal';
+file.path = 'D:\Documents\Unif\PhD\2019-Data\08 - Aug\test3D Fitting\1p';
+file.ext  = '.ome.tif';
+
+focusPlane = 4;%=2 af
+width.xy = 0; %for fitting (3 for 200nm beads, 400 nm beads to be determined, 0 to let the code find the width)
+width.z  = 0;
+nParticles = 1;%number of particles expected in the movie has to be exact
+minDist = 3; %in pixels (min distant expected between particles
+pxSize = 95;%in nm
+cropRadius = 50; %cut the frame to reduce the amount of data to be fitted
+info.type = 'normal';%Transmission or normal movie
+info.runMethod = 'load';
+info.calibrate = false;
+toAnalyze = '.ome.tif';%accepted: .mp4, .ome.tif, folder. (folder that contain folders of .ome.tif.
+outputFolder = 'Results';%name of the folder to output the results
+%% Loading
+%we get the zCalibration directory
+folder2Mov = dir(file.path);
+folder2Mov = folder2Mov(cell2mat({folder2Mov.isdir}));
+%loop through the content of the directory
+mov = struct();
+count = 0;
+for i = 3:size(folder2Mov,1)
+    %Check if the directory
+    folderPath = [folder2Mov(i).folder filesep folder2Mov(i).name];
+    file2Analyze = Core.Movie.getFileInPath(folderPath,file.ext);
+
+    if ~isempty(file2Analyze)
+        count = count+1;
+        filetmp.path = file2Analyze.folder;
+        filetmp.ext  = file.ext;
+        tmp = Core.MPMovie(filetmp , path2Cal,info);
+        
+        if count == 1
+            tmp.giveInfo;
+        else
+            tmp.info = mov.g1.getInfo; 
+        end
+        tmp.calibrate;
+        mov.(['g' num2str(i-2)]) = tmp;
+
+
+    else
+
+        warning([folder2Mov(i).folder filesep folder2Mov(i).name ' did not contain any ome.Tif and is therefore ignored']);
+
+    end
+
+end
+disp('=======> DONE ! <========')
+mov.g1.showFrame(1,5);
+
+%% detection of the center of the beads
+
+%preallocate memory for storing data of the different files
+allData = struct('fileName',[],'traces',[]);
+allData(length(mov)).locPos = [];
+fields = fieldnames(mov);
+frame = 35;
+
+for i = 1:length(fields)
+    currMov = mov.(fields{i});
+    %check detection
+    fullStack = currMov.getFrame(frame);
+    %revert the intensity scale
+    if strcmpi(info.type,'transmission')
+        testFrame = imcomplement(fullStack);
+    else
+        testFrame = fullStack;
+    end
+
+    frame = 5;
+    %get the n maxima where n is the number of particles expected and
+    %minDist is the distance expected between them
+    [pos] = goldProj.nMaxDetection (testFrame(:,:,focusPlane),nParticles,minDist);
+
+    x0 = pos(:,2);
+    y0 = pos(:,1);
+
+    pos = [y0 x0];
+    %calculate center of mass of particles position for cropping later
+    cropPos = round(mean(pos,1));
+    %plot to check that the max detection worked
+    
+    %crop and show the cropping so user can see if it is okay
+    testFrame = testFrame(cropPos(:,1)-cropRadius:cropPos(:,1)+cropRadius,...
+            cropPos(:,2)-cropRadius:cropPos(:,2)+cropRadius,:);
+    figure
+    imagesc(testFrame(:,:,focusPlane))
+    hold on
+    plot(pos(:,2),pos(:,1),'r+')
+    
+    
+    %% Fitting
+    nFrames = currMov.raw.movInfo.maxFrame(1);
+    %get the X Y dommain
+    x = 1:size(testFrame,2);
+    y = 1:size(testFrame,1);
+    z = 1:size(testFrame,3);
+    [domX,domY,domZ] = meshgrid(x,y,z);
+    dom(:,:,:,1) = domX;
+    dom(:,:,:,2) = domY;
+    dom(:,:,:,3) = domZ;
+    %preallocate memory
+       
+    data2Store = zeros(nFrames,3,nParticles);
+    fitMov = zeros(100,100,nFrames);
+    h = waitbar(0,'Fitting Data');%create waiting bar
+    %unSortedData =[];
+    planePos = currMov.calibrated.oRelZPos;
+    widths.xy = zeros(1,nFrames);
+    widths.z  = zeros(1,nFrames);
+    for j = 1:nFrames
+        currentFrame = double(currMov.getFrame(j));
+        currentFrame = currentFrame(cropPos(:,1)-cropRadius:cropPos(:,1)+cropRadius,...
+            cropPos(:,2)-cropRadius:cropPos(:,2)+cropRadius,:);
+        %inital detection of particles on currentFrame
+        [pos] = goldProj.nMaxDetection (currentFrame(:,:,focusPlane),nParticles,minDist);
+ 
+        x0 = pos(:,2);
+        y0 = pos(:,1);
+        [~,idx] = max(max(max(currentFrame)));
+        z0 = ones(size(x0))*idx;
+        
+        %Multiple gaussian fitting occurs here
+        [gPar,resnorm,res] = Localization.Gauss.MultipleGFit3D(currentFrame,x0,y0,z0,dom,nParticles,width); 
+        
+        widths.xy(j) = gPar(2);
+        widths.z(j) = gPar(3);
+        g = reshape(gPar(5:end),[3,nParticles]);
+        g = g';
+        
+        nPart = size(x0,1);
+        z = g(:,3);
+        %Get Z position
+        for k = 1:nPart
+            
+            domain = 1:size(currentFrame,3);
+
+            if or(z(k)<min(domain),z(k)>max(domain))
+                z(k)   = NaN;                           
+            else
+                tmpZ = floor(z(k));
+                fracZ = z(k)-tmpZ;
+                z(k) = planePos(tmpZ)+fracZ*(planePos(tmpZ+1) - planePos(tmpZ));
+                z(k) = z(k)*1000;
+            end
+            
+        end
+        
+        if j>1
+            %Tracking based on MSD minimization 
+            newOrder = goldProj.simpleTracking(gPar(5:end),prevPos,'3D');
+            %reshaping to format the final data and sorting with new order
+            gPos = reshape(gPar(5:end),[3,nParticles]);
+            gPos = gPos(:,newOrder);
+            prevPos = reshape(gPos,[1,nParticles*3]);
+            gPos = reshape(gPos,[1,3,nParticles])*pxSize;
+            gPos(:,3,:) = z;
+            %store data
+            data2Store(j,:,:) = gPos;
+            clear gPos
+
+        else
+            %First frame we just reshape
+            gPos = reshape(gPar(5:end),[3,nParticles]);
+            gPos = reshape(gPos,[1,3,nParticles])*pxSize;
+            gPos(:,3,:) = z;
+            data2Store(j,:,:) = gPos;
+            %store the gPar in the prev for checking particle order
+            prevPos = gPar(5:end);
+            clear gPos
+        end
+        %update waitbar value
+        waitbar(j/nFrames,h,'Fitting Data')
+    end
+    
+    %clear waitbar
+    close(h);
+    disp(['The Average fitted width in XY is: ' num2str(mean(widths.xy))])
+    disp(['The Average fitted width in z is: ' num2str(mean(widths.z))])
+    currentPath = currMov.raw.movInfo.Path;
+    %save data to the current folder being analyze
+    filename = [currentPath filesep 'LocalizationData.mat'];
+    save(filename,'data2Store');
+    %store data in allData
+    allData(i).traces = data2Store;
+    allData(i).fileName = currentPath;  
+  
+
+end
+
+%% convert Data to table
+figure
+hold on
+for i = 1: size(data2Store,3)
+    plot3(data2Store(:,1,i),data2Store(:,2,i),data2Store(:,3,i));
+    
+end
+axis image
+view(3)
+for i =1: size(allData,2)
+    traces = cell(nParticles,1);
+    
+    for j = 1: nParticles
+        tabData = table(zeros(nFrames,1),zeros(nFrames,1),zeros(nFrames,1),...
+            zeros(nFrames,1),'VariableNames',{'row','col','z','t'});
+        
+        tabData.col = allData(i).traces(:,1,j);
+        tabData.row = allData(i).traces(:,2,j);
+        tabData.z = allData(i).traces(:,3,j);
+        tabData.t = (1:nFrames)';
+        
+        traces{j} = tabData;
+    end
+    allData(i).traces = traces;
+end
+trackRes = allData;
+filename = [file.path filesep 'trackRes.mat'];
+save(filename,'trackRes');
+h = msgbox('Data succesfully saved');
